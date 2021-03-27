20d7c3ba2d1388919185b9c6545dc713d3514e0f
Marina Monsivais <minamonsi@me.com>
2021-03-26 21:50:33 -0500

    Completed validation for Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import * as yup from "yup";
+import { schema } from "./FormSchema";
 
 export default function Login(){
-    const [credential, setCredential] = useState(
-        { username: '', password:''}
-    )
+    const [credential, setCredential] = useState({ username: '', password:''})
+    const [errors, setErrors] = useState({ username: '', password:''})
+    const [disabled, setDisabled] = useState(true)
+    const setCredentialErrors = (name, value) => {
+        yup.reach(schema, name).validate(value)
+        .then(() => setErrors({...errors, [name]: ''}))
+        .catch(err => setErrors({...errors, [name]: err.errors[0]}))
+    }
+
+
     const[authCode, setAuthCode]= useState('')
 
+
     const handleChange = event => {
+        const { name, value, type} = event.target;
+        const valueToUse = event.target.value;
         setCredential({...credential, [event.target.name]: event.target.value});
+        setCredentialErrors(name, valueToUse)
+        setCredential({...credential, [name]: valueToUse})
     }
 
     const onSubmit = event => {
@@ -15,8 +29,16 @@ export default function Login(){
         console.log(credential)
     }
 
+    useEffect(() => {
+        schema.isValid(credential).then(valid => setDisabled(!valid))
+    }, [credential])
+
     return (
         <div className="loginForm">
+            <div style={{ color : 'red' }}>
+                <div>{errors.username}</div>
+                <div>{errors.password}</div>
+            </div>
     
             <form onSubmit={onSubmit}>
             <h1>Log In</h1>
@@ -36,7 +58,7 @@ export default function Login(){
             </div>
 
             <div>
-                <button>Log In</button>
+                <button disabled={disabled}>Log In</button>
             </div>
             
             </form>
