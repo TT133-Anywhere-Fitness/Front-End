3d2447d8b89fa98df02dacab240035f393cc888d
Allison Castaneda <allraec@yahoo.com>
2021-03-27 19:58:25 -0500

    login auth finished

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React from "react";
-import { BrowserRouter, Route, Link, Switch} from 'react-router-dom';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 import './App.css';
 import SignUp from './components/SignUp'
 import Login from './components/Login'
 
 function App() {
   return (
-    <BrowserRouter>
     <div className="App">
-      <div>
-        <Link to='/' style={{ padding: 5, textDecoration: 'none'}}>Home</Link>
-
-        <Link to='/signup' style={{ padding: 5, textDecoration: 'none'}}>Sign Up</Link>
-
-        <Link to='/login' style={{ padding: 5, textDecoration: 'none'}}>Login</Link>
-      </div>
-
-      <Switch>
-        <Route path='/signup' component={SignUp}/>
-        <Route path='/login' component={Login}/>
-      </Switch>
-
-    </BrowserRouter>
+      <Router>
+        <Switch>
+          <Route path='/signup' component={SignUp}/>
+          <Route component={Login}/>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
